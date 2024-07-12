af04897f238924a3d6d9d2cd69830607d852f55f
Pedro Henrique <phlferezin03@gmail.com>
2024-07-12 11:18:02 -0300

    feat: add space in home form

--- lib/widgets/home/home_form_widget.dart
+++ lib/widgets/home/home_form_widget.dart
@@ -97,7 +97,7 @@ class _HomeFormWidgetState extends State<HomeFormWidget> {
                 ImageConstants.shoppingBags,
                 height: 140,
               ),
-              const SizedBox(height: 20),
+              const SizedBox(height: 80),
             ],
           ),
         ),
